8307cb2139f65d87f9c91f0faa04371cbe360272
Bilal <lodhc2@gmail.com>
2024-11-17 16:50:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Learning
+{     //Explicit Interface Implimentation
+    interface I1
+    {
+        void InterfaceMethod();
+    }
+    interface I2
+    {
+        void InterfaceMethod();
+    }
+    public class Program : I1, I2
+    {
+        void I1.InterfaceMethod()
+        {
+            Console.WriteLine("I1 Interface Method");
+        }
+        void I2.InterfaceMethod()
+        {
+            Console.WriteLine("I2 Interface Method");
+        }
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            ((I1)P).InterfaceMethod();
+            ((I2)P).InterfaceMethod();
+
+        }
+    }
+}
